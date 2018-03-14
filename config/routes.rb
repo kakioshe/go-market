6aa73741bba2112f79d913c3b4f4b3ba0647d5ed
s4511443 <louis.kakioshe@gmail.com>
2018-03-14 10:23:38 +1000

    add an example login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'login#index'
-  get '/login' => 'login#index'
+  root 'pages#index'
+  get '/login' => 'sessions#new'
+  get '/register' => 'registrations#new'
+  post '/register' => 'registrations#create'
 end
