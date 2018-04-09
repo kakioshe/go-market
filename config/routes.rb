3580005fd8c80993a297c76c5a835c5bc11ddfa2
Louis Christopher <louis.kakioshe@gmail.com>
2018-04-09 04:15:34 +0000

    Revert "Revert "Merge branch 'storepage' into 'development'""
    This reverts commit 6629d564c892402501231f761471dee8ef59e745

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+
+  resources :products do
+    resources :pictures
+  end
+
+
+get '/catalogue', to: 'products#index'
+resources :users, only: [:show]
+resources :stores, only: [:create, :new, :show]
 end
