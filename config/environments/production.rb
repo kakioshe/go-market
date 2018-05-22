cef9170ffb6e5c055488a9748db4888057b05f52
s4511443 <louis.kakioshe@gmail.com>
2018-05-23 09:54:29 +1000

    modify production.rb change email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,17 +70,28 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
 
-  ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:        'https://go-market.herokuapp.com/',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
   }
+  #config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
+
+ # ActionMailer::Base.smtp_settings = {
+ #     :address        => 'smtp.sendgrid.net',
+ #     :port           => '587',
+ #     :authentication => :plain,
+ #     :user_name      => ENV['SENDGRID_USERNAME'],
+ #     :password       => ENV['SENDGRID_PASSWORD'],
+ #     :domain         => 'heroku.com',
+ #     :enable_starttls_auto => true
+ # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
