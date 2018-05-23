80b56f8d81ee9c849d14d87606cf0be85efb1063
s4511443 <louis.kakioshe@gmail.com>
2018-05-24 03:57:02 +1000

    modify development parameter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
-    domain:        'https://go-market.herokuapp.com/',
+    domain:         ENV['MAIL_HOST'] ,
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
