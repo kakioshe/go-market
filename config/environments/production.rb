55830dc8f45916631d148ce0becaf592eb7fe3d7
s4511443 <louis.kakioshe@gmail.com>
2018-05-23 10:33:25 +1000

    modify domain and email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.default_url_options = { host: 'go-market.herokuapp.com' }
 
 
   ActionMailer::Base.smtp_settings = {
