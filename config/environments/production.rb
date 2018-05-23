38c3f019493ee3f1b1b800ae55eddd1d74743929
s4511443 <louis.kakioshe@gmail.com>
2018-05-23 10:16:36 +1000

    Modify email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,15 +72,18 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:        'https://go-market.herokuapp.com/',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
+
+
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.gmail.com',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDMAIL_USERNAME'],
+      :password       => ENV['SENDMAIL_PASSWORD'],
+      :domain         => 'gmail.com',
+      :enable_starttls_auto => true
   }
+
   #config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
 
  # ActionMailer::Base.smtp_settings = {
